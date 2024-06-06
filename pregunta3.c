b36b1ff29084423aa072ab49a786d75422542853
Eric Bracamonte <71293126+Ereiclo@users.noreply.github.com>
2024-06-05 22:25:53 -0500

    Update pregunta3.c

--- pregunta3.c
+++ pregunta3.c
@@ -4,26 +4,41 @@
 #define IDLE 2
 
 #define WATERING_TIME 3000
-
+#define IDLE_TIME 3000
+#include <LiquidCrystal.h>
 
 int STATE = READING;
-int TIMEOUT_END = 0;
-int WATERING_END = 0;
+long TIMEOUT_END = 0;
+long WATERING_END = 0;
 
 float humidity = 0;
 
 #define BLUE_PIN 2
 #define RED_PIN 3
+#define PIN_MOTOR 44
 #define PIN_BUZZER 9
 #define PIN_HUMEDAD A0
 
 
+const int rs = 33, en = 31, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup()
 {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.clear();
+  
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_HUMEDAD, INPUT);
+  pinMode(PIN_MOTOR, OUTPUT);
+
+  digitalWrite(BLUE_PIN, HIGH);
+  digitalWrite(RED_PIN, HIGH);
+	noTone(PIN_BUZZER);
+  digitalWrite(PIN_MOTOR, HIGH);
 }
 
 
@@ -31,37 +46,54 @@ void wateringState() {
   //turn on blue light
   //change to IDLE after wait
   digitalWrite(BLUE_PIN, LOW);  
+  digitalWrite(PIN_MOTOR, LOW);  
+  Serial.println(millis());
+  Serial.println(WATERING_END);
   if (millis() > WATERING_END) {
     STATE = IDLE;
-    digitalWrite(BLUE_PIN, HIGH);  
+    TIMEOUT_END =millis()+IDLE_TIME;
+    digitalWrite(BLUE_PIN, HIGH);
+    digitalWrite(PIN_MOTOR, HIGH);  
   }
+  lcd.clear(); 
+  lcd.print(humidity);
 }
 
 void idleState() {
   // wait until timeout
   if (millis() > TIMEOUT_END) {
     STATE = READING;
+    
 	}
+  lcd.clear();
+  lcd.print("IDLE");
 }
 
 void readingState(){
-  if(humidity < 20){
+  if(humidity < 10.f){
     STATE = WATERING;
     WATERING_END = millis() + WATERING_TIME;
   } 
+  lcd.clear(); 
+  lcd.print(humidity);
 }
 
 
 void handleState (){
+  Serial.println(STATE);
+
   switch(STATE){
     case READING:
       readingState();
+  
       break;
     case WATERING:
       wateringState();
+
       break;
   	case(IDLE):
       idleState();
+
       break;
   }
 }
@@ -70,7 +102,7 @@ void handleState (){
 
 
 void handleWarning() {
-  if(humidity > 80.) {
+  if(humidity > 50.) {
     digitalWrite(RED_PIN, LOW);
     tone(PIN_BUZZER, 1000); // Send 1KHz sound signal...
   }
@@ -82,7 +114,14 @@ void handleWarning() {
 
 
 void readHumidity() {
-  humidity = (analogRead(PIN_HUMEDAD)*100.0/1023.0);
+  float rawHumidity= analogRead(PIN_HUMEDAD);
+  humidity = 100.0- (rawHumidity*100.0/1023.0);
+
+  //Serial.println(humidity);
+
+  
+  
+  
 }
 
 void loop()
